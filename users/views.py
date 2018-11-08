5f92463625ee3be087af783549f74974289982b4
Kris Spencer <antisocialtrees@gmail.com>
2018-11-08 00:29:03 -0800

    Create view framework

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def view_profile(request, user_pk):
+    pass
+
+
+def edit_profile(request, user_pk):
+    pass
