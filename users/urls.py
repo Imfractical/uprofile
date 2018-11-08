13f76d4768f809b308f10231109b491623678c05
Kris Spencer <antisocialtrees@gmail.com>
2018-11-08 00:28:03 -0800

    Fix URLs

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
-"""users app URL configuration"""
-
 from django.urls import path
 
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('<user_pk>/', views.profile, name='profile'),
+    path('<user_pk>/', views.view_profile, name='view_profile'),
+    path('<user_pk>/edit/', views.edit_profile, name='edit_profile')
 ]
