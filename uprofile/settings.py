7ac865bd0a25c17a596c6dd363221e97b7f058ee
Kris Spencer <antisocialtrees@gmail.com>
2018-11-10 21:02:50 -0800

    Delete user app, make accounts app (that extends Django's User class)

--- uprofile/settings.py
+++ uprofile/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class UserRegistrationForm(forms.Form):
-    email = forms.EmailField()
-    email_confirmation = forms.EmailField()
-    password = forms.CharField(min_length=14, max_length=100, widget=forms.PasswordInput)
-    password_confirmation = forms.CharField()
-    dob = forms.DateField()
