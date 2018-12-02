48f8ac142205568c817f6886916201058ac9f9ab
Kris Spencer <antisocialtrees@gmail.com>
2018-12-02 08:10:31 -0800

    Added necessary urls for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('profile/<int:user_pk>', views.profile, name='profile'),
 ]
