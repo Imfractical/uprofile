83007ad31174d7799fa863b8523df22110b87702
Kris Spencer <antisocialtrees@gmail.com>
2018-12-02 07:53:27 -0800

    Add a success message for signing up

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 
 from .forms import UserCreationForm
+from .models import User
 
 
 def signup(request):
@@ -9,13 +11,21 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            email = form.cleaned_data.get('email1')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(email=email, password=password)
-            login(request, user)
+            email = form.cleaned_data['email1']
+            password = form.cleaned_data['password1']
+            login(request, authenticate(email=email, password=password))
+            user = User.objects.get(email=email)
+            messages.success(
+                request,
+                "User registration successful! Welcome, {}".format(user.first_name)
+            )
 
-            return redirect('accounts:signup')
+            return redirect('accounts:profile', args=[user.id])
     else:
         form = UserCreationForm()
 
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+def signin(request):
+    pass
